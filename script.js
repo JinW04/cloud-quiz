88959d1a1d48ca270a8d229ed66eda1dd8b77d04
Jin <jinssu@hotmail.com>
2026-03-06 10:55:31 +0100

    Added 10 trivia questions that randomizes order

--- script.js
+++ script.js
@@ -56,12 +56,93 @@ let score = 0;
 // Questiosn and answers
 const questions = [
     {
-        question: "PLACEHOLDER: What is 2 + 2?",
+        question: "What is the food that can NEVER go bad(expire)?",
         answers: [
-            { text: "3", correct: false },
-            { text: "4", correct: true },
-            { text: "5", correct: false },
-            { text: "22", correct: false }
+            { text: "Uncooked white rice", correct: false },
+            { text: "Honey", correct: true },
+            { text: "Pasta", correct: false },
+            { text: "Canned food", correct: false }
+        ]
+    },
+    {
+                question: "What is the hardest natural substance on Earth?",
+        answers: [
+            {text: "Diamond", correct: true},
+            {text: "Obsidian", correct: false},
+            {text: "Ruby", correct: false},
+            {text: "Topaz", correct: false}
+        ]
+    },
+        {
+                question: "How many cards are in a standard deck of playing cards?",
+        answers: [
+            {text: "30", correct: false},
+            {text: "28", correct: false},
+            {text: "32", correct: true},
+            {text: "34", correct: false}
+        ]
+    },
+        {
+                question: "What is James Bond's code name?",
+        answers: [
+            {text: "07", correct: false},
+            {text: "700", correct: false},
+            {text: "0007", correct: false},
+            {text: "007", correct: true}
+        ]
+    },
+        {
+                question: "Which country is the largest in the world?",
+        answers: [
+            {text: "Russia", correct: true},
+            {text: "China", correct: false},
+            {text: "America", correct: false},
+            {text: "Canada", correct: false}
+        ]
+    },
+        {
+                question: "What travels the fastest out of the 4 options?",
+        answers: [
+            {text: "Sound", correct: false},
+            {text: "Fighterjets", correct: false},
+            {text: "Light", correct: true},
+            {text: "A car", correct: false}
+        ]
+    },
+        {
+                question: "How many elements are in the periodic table?",
+        answers: [
+            {text: "120", correct: false},
+            {text: "118", correct: true},
+            {text: "181", correct: false},
+            {text: "108", correct: false}
+        ]
+    },
+        {
+                question: "How many colors are in a rainbow?",
+        answers: [
+            {text: "6", correct: false},
+            {text: "7", correct: true},
+            {text: "4", correct: false},
+            {text: "8", correct: false}
+        ]
+    },
+        {
+                question: "What fish species is Nemo?",
+        answers: [
+            {text: "Goldfish", correct: false},
+            {text: "Nemo", correct: false},
+            {text: "Silverfish", correct: false},
+            {text: "Clownfish", correct: true}
+        ]
+    },
+        {
+                question: "What animal has the biggest eyes?",
+        answers: [
+            {text: "Colossal squid", correct: true},
+            {text: "Blue whale", correct: false},
+            {text: "African elephant", correct: false},
+            {text: "Ostrich", correct: false}
         ]
     }
 ];
@@ -70,6 +151,7 @@ const questions = [
 function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
+    shuffleArray(questions)
     nextButton.style.display = "none"; 
     showQuestion();
 }
@@ -136,3 +218,11 @@ nextButton.addEventListener("click", () => {
         nextButton.addEventListener("click", startQuiz); 
     }
 });
+
+// Shuffles the questions
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
